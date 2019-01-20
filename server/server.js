b9152647c3f37f187cf5e3a487594a8361faab8b
Awale, Gaurav <Gaurav.Awale@cdk.com>
2019-01-20 20:53:52 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+let port = process.env.PORT || 3000;
+let app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+    console.log('Stared on port ', port);
+});
+
